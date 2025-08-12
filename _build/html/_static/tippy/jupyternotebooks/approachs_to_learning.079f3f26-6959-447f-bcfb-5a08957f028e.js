bd0175f0bab4d656795d13ffbe5136980bec8d80
PeterMarshall29 <p.j.marshall@lboro.ac.uk>
2025-08-12 16:48:47 +0100

    with quiz attemmpt

---
+++ _build/html/_static/tippy/jupyternotebooks/approachs_to_learning.079f3f26-6959-447f-bcfb-5a08957f028e.js
@@ -0,0 +1,22 @@
+selector_to_html = {"a[href=\"#how-to-learn-c\"]": "<h1 class=\"tippy-header\" style=\"margin-top: 0;\">How to Learn C++<a class=\"headerlink\" href=\"#how-to-learn-c\" title=\"Link to this heading\">#</a></h1><p>Growth mind set bit</p>"}
+skip_classes = ["headerlink", "sd-stretched-link"]
+
+window.onload = function () {
+    for (const [select, tip_html] of Object.entries(selector_to_html)) {
+        const links = document.querySelectorAll(` ${select}`);
+        for (const link of links) {
+            if (skip_classes.some(c => link.classList.contains(c))) {
+                continue;
+            }
+
+            tippy(link, {
+                content: tip_html,
+                allowHTML: true,
+                arrow: true,
+                placement: 'auto-start', maxWidth: 500, interactive: false, theme: 'light', duration: [200, 100], delay: [200, 500],
+
+            });
+        };
+    };
+    console.log("tippy tips loaded!");
+};
