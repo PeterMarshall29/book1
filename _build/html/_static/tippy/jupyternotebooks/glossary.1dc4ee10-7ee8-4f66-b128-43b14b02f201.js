9c5922471ba98c9bbb05d0056ac446c96ef3e8c9
PeterMarshall29 <p.j.marshall@lboro.ac.uk>
2025-08-15 18:10:13 +0100

    update 15Aug22

--- _build/html/_static/tippy/jupyternotebooks/glossary.1dc4ee10-7ee8-4f66-b128-43b14b02f201.js
+++ _build/html/_static/tippy/jupyternotebooks/glossary.1dc4ee10-7ee8-4f66-b128-43b14b02f201.js
@@ -1,4 +1,4 @@
-selector_to_html = {"a[href=\"#glossary\"]": "<h1 class=\"tippy-header\" style=\"margin-top: 0;\">Glossary<a class=\"headerlink\" href=\"#glossary\" title=\"Link to this heading\">#</a></h1><p class=\"glossary\">term Declaration</p><p>:Declarations introduce names into a C++ program. Each kind of entity is declared differently.</p>"}
+selector_to_html = {"a[href=\"#term-Declaration\"]": "<dt id=\"term-Declaration\">Declaration</dt><dd><p>Declarations introduce names into a C++ program. Each kind of entity is declared differently.</p></dd>", "a[href=\"#glossary\"]": "<h1 class=\"tippy-header\" style=\"margin-top: 0;\">Glossary<a class=\"headerlink\" href=\"#glossary\" title=\"Link to this heading\">#</a></h1>", "a[href=\"#term-Definition\"]": "<dt id=\"term-Definition\">Definition</dt><dd><p>Definitions are declarations that are sufficient to use the entity identified by the name.</p></dd>"}
 skip_classes = ["headerlink", "sd-stretched-link"]
 
 window.onload = function () {
