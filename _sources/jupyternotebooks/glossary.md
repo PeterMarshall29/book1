7af3c86d3e4e80f8b28d1f06fab34d1fb6ec1b12
PeterMarshall29 <p.j.marshall@lboro.ac.uk>
2025-08-15 14:52:26 +0100

    Update documentation

---
+++ _sources/jupyternotebooks/glossary.md
@@ -0,0 +1,26 @@
+---
+jupytext:
+  formats: md:myst
+  text_representation:
+    extension: .md
+    format_name: myst
+kernelspec:
+  display_name: C++ 20
+  language: c++
+  name: cpp20
+---
+
+# Glossary
+
+{.glossary}
+
+term Declaration
+
+:Declarations introduce names into a C++ program. Each kind of entity is declared differently.
+
+term Definition
+
+:Definitions are declarations that are sufficient to use the entity identified by the name.
+
+
+
--- _static/tippy/jupyternotebooks/the_cpp_language.37ffc1a4-9a65-476e-a918-8c3babdcf35e.js
+++ _static/tippy/jupyternotebooks/the_cpp_language.37ffc1a4-9a65-476e-a918-8c3babdcf35e.js
@@ -1,4 +1,4 @@
-selector_to_html = {"a[href=\"#the-c-language\"]": "<h1 class=\"tippy-header\" style=\"margin-top: 0;\">The C++ Language<a class=\"headerlink\" href=\"#the-c-language\" title=\"Link to this heading\">#</a></h1><p>Start with ideas like syntax\u2026</p><p>All the `C++ language syntax is detailed here: <a class=\"reference external\" href=\"https://en.cppreference.com/w/\">cppreference.com</a></p>"}
+selector_to_html = {"a[href=\"#the-c-language\"]": "<h1 class=\"tippy-header\" style=\"margin-top: 0;\">The C++ Language<a class=\"headerlink\" href=\"#the-c-language\" title=\"Link to this heading\">#</a></h1><p>Start with ideas like syntax\u2026</p><p>All the `C++ language syntax is detailed here: <a class=\"reference external\" href=\"https://en.cppreference.com/w/\">cppreference.com</a></p>", "a[href=\"https://en.cppreference.com/w/\"]": "<p>The Official C++ Standard Reference Documentation</p>", "a[href=\"https://en.cppreference.com/w/cpp/language/history.html\"]": "<p>Dexcription of the evolution of C++ language</p>"}
 skip_classes = ["headerlink", "sd-stretched-link"]
 
 window.onload = function () {
