adc0f2ff84e3dbe9be6c94a3777f98fd44c201d8
PeterMarshall29 <p.j.marshall@lboro.ac.uk>
2026-01-30 13:19:11 +0000

    30jan26_2

---
+++ _build/html/_static/tippy/jupyternotebooks/introduction_to_object_oriented_programming.98e11828-7b68-4d81-9510-cbb3024fb33f.js
@@ -0,0 +1,22 @@
+selector_to_html = {"a[href=\"#oop-in-c\"]": "<h1 class=\"tippy-header\" style=\"margin-top: 0;\">OOP in C++<a class=\"headerlink\" href=\"#oop-in-c\" title=\"Permalink to this heading\">#</a></h1><p>Programs are written to solve specific problems, and for all but the simplest tasks, we can take different approaches, each potentially offering benefits over the others.</p><p>Before the implementation of the basic syntactic statements, we can take different approaches to the overall design of any program.</p>"}
+skip_classes = ["headerlink", "sd-stretched-link"]
+
+window.onload = function () {
+    for (const [select, tip_html] of Object.entries(selector_to_html)) {
+        const links = document.querySelectorAll(` ${select}`);
+        for (const link of links) {
+            if (skip_classes.some(c => link.classList.contains(c))) {
+                continue;
+            }
+
+            tippy(link, {
+                content: tip_html,
+                allowHTML: true,
+                arrow: true,
+                placement: 'auto-start', maxWidth: 500, interactive: false, theme: 'material', duration: [200, 100], delay: [200, 0],
+
+            });
+        };
+    };
+    console.log("tippy tips loaded!");
+};
